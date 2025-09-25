e5fa937df1b2fda740aee0c0f1e9bf8be69d4c9b
jba06165@uga.edu <jba06165@uga.edu>
2025-09-25 00:24:18 -0400

    made admin movie page look better

--- ces/src/app/admin/addMovie/page.js
+++ ces/src/app/admin/addMovie/page.js
@@ -39,53 +39,86 @@ export default function AdminAddMovie() {
   }
 
     return (
+    <main className="min-h-dvh bg-white text-slate-100 flex items-start justify-center p-6">
+      <section className="w-full max-w-4xl bg-white border border-slate-800 rounded-2xl shadow-xl p-6 md:p-8">
       <div>
-      <h1>Add Movie</h1>
+      <h1 className="text-2xl font-bold text-black">Add Movie</h1>
+      <section className="text-black">
       <form onSubmit={handleSubmit}>
-        <label>
+         
+        <label >
           Title
-          <input value={form.title} onChange={e=>update("title", e.target.value)} required />
+          <input 
+          className="w-full rounded-lg border border-black" 
+          value={form.title} onChange={e=>update("title", e.target.value)} required 
+          />
         </label>
         <br/><br/>
 
         <label>
           Poster URL
-          <input value={form.posterUrl} onChange={e=>update("posterUrl", e.target.value)} required />
+          <input 
+          className="w-full rounded-lg border border-black" 
+          value={form.posterUrl} onChange={e=>update("posterUrl", e.target.value)} required 
+          />
         </label>
         <br/><br/>
 
         <label>
           Rating
-          <input value={form.rating} onChange={e=>update("rating", e.target.value)} />
+          <input 
+          className="w-full rounded-lg border border-black" 
+          value={form.rating} onChange={e=>update("rating", e.target.value)}
+          />
         </label>
         <br/><br/>
 
         <label>
           Description
-          <textarea value={form.description} onChange={e=>update("description", e.target.value)} required />
+          <textarea 
+          className="w-full rounded-lg border border-black" 
+          value={form.description} onChange={e=>update("description", e.target.value)} required
+           />
         </label>
         <br/><br/>
 
         <label>
           Show Date
-          <input type="date" value={form.showDate} onChange={e=>update("showDate", e.target.value)} required />
+          <input 
+          className="w-full rounded-lg border border-black" 
+          type="date" 
+          value={form.showDate} onChange={e=>update("showDate", e.target.value)} required
+           />
         </label>
         <br/><br/>
 
         <label>
           Trailer URL
-          <input value={form.trailerUrl} onChange={e=>update("trailerUrl", e.target.value)} required />
+          <input 
+          className="w-full rounded-lg border border-black" 
+          value={form.trailerUrl} onChange={e=>update("trailerUrl", e.target.value)} required 
+          />
         </label>
         <br/><br/>
 
         <label>
           Genre
-          <input value={form.genre} onChange={e=>update("genre", e.target.value)} required />
+          <input 
+          className="w-full rounded-lg border border-black" 
+          value={form.genre} onChange={e=>update("genre", e.target.value)} required
+           />
         </label>
         <br/><br/>
 
-        <button type="submit">Add Movie</button>
+        <button 
+        className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition"
+        type="submit">
+          Add Movie
+        </button>
       </form>
+  </section>
     </div>
+    </section> 
+    </main> 
   );
 }
