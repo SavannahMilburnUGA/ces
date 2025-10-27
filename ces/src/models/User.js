65c80f12d2ce01a375195a2a9d0fb2d68a301ebb
jba06165@uga.edu <jba06165@uga.edu>
2025-10-27 16:48:59 -0400

    additions to create account

--- ces/src/models/User.js
+++ ces/src/models/User.js
@@ -2,27 +2,33 @@ import mongoose from "mongoose";
 import { date } from "zod";
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, trim: true, required: true },
-  email: { type: String, trim: true, unique: true, required: true, lowercase: true },
-  phone: { type: String, trim: true, required: true },              
+  name:         { type: String, trim: true, required: true },
+  email:        { type: String, trim: true, unique: true, required: true, lowercase: true },
+  phone:        { type: String, trim: true, required: true },
   passwordHash: { type: String, required: true },
-  promoOptIn: { type: Boolean, default: false },                   
-  status: { type: String, enum: ["Inactive", "Active"], default: "Inactive" },
-  confirmationToken: { type: String, index: true },
-  confirmationCodeHash: { type: String },                          
-  confirmationExpires: { type: Date },
-  customerId: { type: String, unique: true, sparse: true },   
-  billingAddress: AddressSchema,
-  payments: {
-  type: [PaymentSchema],
-  validate: {
-    validator: (arr) => !arr || arr.length <= 3,
-    message: "You can store at most 3 payment cards."
-  },
-  default: undefined
-}
 
+  promoOptIn:   { type: Boolean, default: false },
+  status:       { type: String, enum: ["Inactive","Active"], default: "Inactive" },
+
+  
+  homeAddress:  { type: AddressSchema, required: false },  
+
+  // Optional up to 3 payment cards
+  payments: {
+    type: [PaymentSchema],
+    validate: {
+      validator: (arr) => !arr || arr.length <= 3,
+      message: "You can store at most 3 payment cards."
+    },
+    default: undefined
+  },
+
+  confirmationToken:   { type: String, index: true },
+  confirmationCodeHash:{ type: String },
+  confirmationExpires: { type: Date },
+  customerId:          { type: String, unique: true, sparse: true }
 }, { timestamps: true });
+
 
 const AddressSchema = new mongoose.Schema({
   street: { type: String, trim: true },
@@ -31,11 +37,14 @@ const AddressSchema = new mongoose.Schema({
   zip:    { type: String, trim: true }
 }, { _id: false });
 
+
+
 const PaymentSchema = new mongoose.Schema({
   cardType:   { type: String, trim: true },
   cardNumber: { type: String, trim: true },  
-  expiration: { type: String, trim: true },   
-}, { _id: false });
+  expiration: { type: String, trim: true }, 
+  billingAddress:{ type: String, trim: true },
+},{ _id: false });
 
 UserSchema.index({ email: 1 }, { unique: true });
 
