58960bf4d7587137324b2f578985c38e5e3008b0
SavannahMilburnUGA <Savannah.Milburn@uga.edu>
2025-10-27 23:16:30 -0400

    Handle admin delete in manage movies

--- ces/src/app/api/movies/[id]/route.js
+++ ces/src/app/api/movies/[id]/route.js
@@ -24,3 +24,27 @@ export async function GET(req, { params }) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
+
+// Delete
+export async function DELETE(req, { params }) {
+  await connectDB();
+  
+  const { id } = await params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return NextResponse.json({ error: "Invalid movie ID" }, { status: 400 });
+  } // if 
+
+  try {
+    const _id = new mongoose.Types.ObjectId(id);
+    const movie = await Movie.findByIdAndDelete(_id);
+    
+    if (!movie) {
+      return NextResponse.json({ error: "Movie not found" }, { status: 404 });
+    } // if 
+
+    return NextResponse.json({ message: "Movie deleted successfully" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  } // try-catch 
+} // DELETE 
