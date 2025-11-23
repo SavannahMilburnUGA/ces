9ffd6e77aa4a659f424a10884185b1d7bcd1461a
SavannahMilburnUGA <Savannah.Milburn@uga.edu>
2025-11-23 14:08:59 -0500

    Admin - Commit 02: Promo model created for MongoDB

--- ces/src/app/admin/manage-promos/page.js
+++ ces/src/app/admin/manage-promos/page.js
@@ -1,4 +1,18 @@
 // Manage Promos Home page for Admin 
+
+
+// Allow Admin to Add Promos
+// Add Promo button - Promo Form: promoCode, discountPercent, startDate, endDate
+
+// Send email promos to registed users who have subscribed for promos:
+// Send Email button - confirmation
+// Need to find all users who opted in. 
+
+// Registed Users can subscribe/unsubscribe - check Edit Profile. 
+
+// Allow customers to use promo codes. 
+// /models/Promo.js: promoCode, discountPercent, startDate, endDate, isActive
+
 "use client";
 
 export default function ManagePromos () {
