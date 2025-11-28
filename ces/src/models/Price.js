76da33fe3513068f6f5988b3027091db86f0dbc7
SavannahMilburnUGA <Savannah.Milburn@uga.edu>
2025-11-28 17:05:58 -0500

    Order Total - Commit 03: Debug Price.js + check GET request

--- ces/src/models/Price.js
+++ ces/src/models/Price.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
-const PriceSchema = new mongoose.Schema(
+const priceSchema = new mongoose.Schema({
   ticketPrices: {
-    Adult: { type: Number, required: true, default: 12.00, min: 0 },
-    Child: { type: Number, required: true, default: 8.00, min: 0 },
-    Senior: { type: Number, required: true, default: 10.00, min: 0 }
-  },
-  bookingFee: { type: Number, required: true, default: 1.50, min: 0 }, 
-  taxRate: { type: Number, required: true, default: 0.07, min: 0, max: 1 }
-  { timestamps: true }
-);
+    Adult: { type: Number, required: true, min: 0, default: 12.00 },
+    Child: { type: Number, required: true, min: 0, default: 8.00 },
+    Senior: { type: Number, required: true, min: 0, default: 10.00 }},
+  bookingFee: { type: Number, required: true, min: 0, default: 1.50 },
+  taxRate: { type: Number, required: true, min: 0, max: 1, default: 0.07}
+  }, {  
+    timestamps: true
+}); 
 
-export default mongoose.models.Price || mongoose.model("Price", PriceSchema);
+const Price = mongoose.models.Price || mongoose.model("Price", priceSchema);
+
+export default Price;
